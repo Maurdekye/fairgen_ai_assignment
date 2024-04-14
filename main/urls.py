1ce3e44690d4cd9fcc6cfd4aaa55ac896f1c6321
Maurdekye <ncolaprete@gmail.com>
2024-04-14 19:30:25 +0000

    scrapped django based framework

--- main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('create-room/', views.create_room, name='create_room'),
-    path('update-room/<int:room_id>/', views.update_room, name='update_room'),
-    path('create-time/', views.create_time, name='create_time'),
-    path('update-time/<int:time_id>/', views.update_time, name='update_time'),
-]
